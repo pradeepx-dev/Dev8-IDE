98730c231623585d6d320c3adcf89b85f1e88ffa
Pradeep Kumar Maurya <pradeepx135@gmail.com>
2025-05-13 08:15:15 +0530

    Update app.js
    add vercel link in cors

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(cors());
+app.use(cors(
+  origin: ['https://dev8-steel.vercel.app'], // allow Vercel
+  credentials: true
+));
 app.use(express.static("client/dist")); // or "build"
 
 
